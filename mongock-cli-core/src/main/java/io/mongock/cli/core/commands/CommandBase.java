8d397362a712190a4ba0bca1fc4350e2a5edc352
antoniop perez dieppa <aperezdieppa@gmail.com>
2022-01-21 16:21:49 +0000

    checking builder in command

--- mongock-cli-core/src/main/java/io/mongock/cli/core/commands/CommandBase.java
+++ mongock-cli-core/src/main/java/io/mongock/cli/core/commands/CommandBase.java
@@ -1,5 +1,6 @@
 package io.mongock.cli.core.commands;
 
+import io.mongock.api.exception.MongockException;
 import io.mongock.runner.core.builder.RunnerBuilder;
 import java.util.concurrent.Callable;
 
@@ -13,8 +14,11 @@ public abstract class CommandBase<T> implements Callable<T> {
 
     @Override
     public T call() throws Exception {
+        if(builder == null) {
+            throw new MongockException("Mongock builder needs to be provided for this command");
+        }
         return null;
     }
 
-    abstract T execution() throws Exception;
+    public abstract T execution() throws Exception;
 }
