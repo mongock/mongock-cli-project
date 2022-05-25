684ac5f6e54728c6b1893cb32e51d2982fcd7a04
antoniop perez dieppa <aperezdieppa@gmail.com>
2022-05-25 21:18:35 +0100

    resolving conflicts

---
+++ mongock-cli/src/main/java/io/mongock/cli/wrapper/util/JarFactory.java
@@ -0,0 +1,29 @@
+package io.mongock.cli.wrapper.util;
+
+public class JarFactory {
+    private final String jarsLib;
+    private final String cliVersion;
+    private final String mongockCommunityVersion;
+    private final String mongockProVersion;
+
+
+    public JarFactory(String jarsLib, String cliVersion, String mongockCommunityVersion, String mongockProVersion) {
+        this.jarsLib = jarsLib;
+        this.cliVersion = cliVersion;
+        this.mongockCommunityVersion = mongockCommunityVersion;
+        this.mongockProVersion = mongockProVersion;
+    }
+
+    public String cliSpringboot() {
+        return String.format("%s/mongock-cli-springboot-%s.jar", jarsLib, cliVersion);
+    }
+
+    public String cliCore() {
+        return String.format("%s/mongock-cli-core-%s.jar", jarsLib, cliVersion);
+    }
+
+    public String funnerCore() {
+        return String.format("%s/mongock-runner-core-%s.jar", jarsLib, mongockCommunityVersion);
+    }
+
+}
