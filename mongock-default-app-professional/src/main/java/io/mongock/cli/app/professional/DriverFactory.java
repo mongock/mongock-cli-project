79e183807343dc52f9aa6a5f1d65865d26e37675
antoniop perez dieppa <aperezdieppa@gmail.com>
2022-06-15 08:06:55 +0100

    minor refactor around DriverWrpperReceiver

--- mongock-default-app-professional/src/main/java/io/mongock/cli/app/professional/DriverFactory.java
+++ mongock-default-app-professional/src/main/java/io/mongock/cli/app/professional/DriverFactory.java
@@ -8,18 +8,15 @@ public final class DriverFactory {
     private DriverFactory() {
     }
 
-    public static ConnectionDriver getDriver(String driverWrapperName) {
+    public static ConnectionDriver getDriver(DriverWrapper driverWrapper) {
 
-        if(driverWrapperName == null || driverWrapperName.isEmpty()) {
-            throw new RuntimeException("Driver must not be null");
-        }
-        switch (DriverWrapper.valueOf(driverWrapperName)) {
+        switch (driverWrapper) {
             case MONGODB_SPRING_DATA_V3:
                 return new io.mongock.driver.cli.wrapper.mongodb.springdata.v3.SpringDataMongoV3DriverProvider().getDriver();
             case MONGODB_SPRING_DATA_V2:
                 return new io.mongock.driver.cli.wrapper.mongodb.springdata.v2.SpringDataMongoV2DriverProvider().getDriver();
             default:
-                throw new RuntimeException(String.format("%s not found", driverWrapperName));
+                throw new RuntimeException(String.format("%s not found", driverWrapper));
         }
     }
 }
